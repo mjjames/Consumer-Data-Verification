045e57642d9a091f4b3503bdd70859b3fdce4905
Michael James <michael.james@mksoftwaresolutions.co.uk>
2013-09-26 10:24:21 -0100

    new mkpaymentvalidation which only does creditcard checks and simply performs basic validation and a luhn check on the number

---
+++ ConsumerDataVerificationService.Tests/MKPaymentValidationServiceTests.cs
@@ -0,0 +1,66 @@
+﻿using System.Threading.Tasks;
+using MKS.ConsumerDataVerification.PaymentValidation;
+using Xunit;
+
+namespace ConsumerDataVerificationService.Tests
+{
+    public class MKPaymentValidationServiceTests
+    {
+        [Fact]
+        public async Task ValidCardNumberIsValid()
+        {
+            var service = new MKPaymentValidationService();
+            var result = await service.CreditCardValidation("1234567812345670");
+            Assert.True(result.IsValid);
+        }
+
+        [Fact]
+        public async Task ValidCardLengthButInvalidCardLunhReturnsInvalid()
+        {
+            var service = new MKPaymentValidationService();
+            var result = await service.CreditCardValidation("1234567812345678");
+            Assert.False(result.IsValid);
+        }
+
+        [Fact]
+        public async Task ValidMaestroCardNumberReturnsCorrectlyFormatted()
+        {
+            var service = new MKPaymentValidationService();
+            var result = await service.CreditCardValidation("675940543258754468");
+            Assert.False(result.IsValid);
+        }
+
+        [Fact]
+        public async Task CardNumberTooShortReturnsInvalid()
+        {
+            var service = new MKPaymentValidationService();
+            var result = await service.CreditCardValidation("456789");
+            Assert.False(result.IsValid);
+        }
+
+        [Fact]
+        public async Task CardNumberTooLongReturnsInvalid()
+        {
+            var service = new MKPaymentValidationService();
+            var result = await service.CreditCardValidation("456789789456789456123456789");
+            Assert.False(result.IsValid);
+        }
+
+        [Fact]
+        public async Task EmptyCardNumberReturnsInvalid()
+        {
+            var service = new MKPaymentValidationService();
+            var result = await service.CreditCardValidation("");
+            Assert.False(result.IsValid);
+        }
+
+        [Fact]
+        public async Task CardNumberWithLettersReturnsInvalid()
+        {
+            var service = new MKPaymentValidationService();
+            var result = await service.CreditCardValidation("67594054AS58754468");
+            Assert.False(result.IsValid);
+        }
+
+    }
+}
---
+++ ConsumerDataVerificationService/PaymentValidation/MKPaymentValidationService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MKS.ConsumerDataVerification.PaymentValidation
+{
+    public class MKPaymentValidationService : IPaymentValidationService
+    {
+        public Task<BankAccountValidationResult> BankAccountValidation(string sortcode, string account)
+        {
+            throw new NotSupportedException();
+        }
+
+        public Task<CreditCardValidationResult> CreditCardValidation(string cardnumber)
+        {
+            var result = new CreditCardValidationResult(cardnumber)
+                {
+                    IsValid = IsCreditCardFormattedCorrectly(cardnumber) && IsLunnCheckValid(cardnumber)
+                };
+            return TaskEx.FromResult(result);
+        }
+
+        private static bool IsCreditCardFormattedCorrectly(string cardnumber)
+        {
+            return !string.IsNullOrWhiteSpace(cardnumber)
+                    && cardnumber.Length >= 12
+                    && cardnumber.Length <= 19
+                    && cardnumber.ToCharArray().All(char.IsNumber);
+        }
+
+        private static bool IsLunnCheckValid(string cardnumber)
+        {
+            if (string.IsNullOrWhiteSpace(cardnumber))
+            {
+                return false;
+            }
+            return cardnumber.ToCharArray().Select((c, i) => (c - '0') << ((cardnumber.Length - i - 1) & 1)).Sum(n => n > 9 ? n - 9 : n) % 10 == 0;
+        }
+    }
+}
